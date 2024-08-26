67ab8cff6749a927a08952b20133a26e5bb8cfdc
윤석규 <greeentea.dev@gmail.com>
2024-08-26 17:00:54 +0900

    app.js 수정

--- canbus-web/app.js
+++ canbus-web/app.js
@@ -1,31 +1,30 @@
-require('dotenv').config();
 
-const express = require('express')
-const path = require('path');
-const app = express()
+  const express = require('express')
+  const path = require('path');
+  const app = express()
 
-// 라우트 정의
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'main', 'main.html'));
-});
+  // 라우트 정의
+  app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'main', 'main.html'));
+  });
 
-app.get('/auth/login', (req, res) => {
-  res.sendFile(path.join(__dirname, 'auth', 'signin', 'signin.html'));
-});
+  app.get('/auth/login', (req, res) => {
+    res.sendFile(path.join(__dirname, 'auth', 'signin', 'signin.html'));
+  });
 
-app.get('/auth/register', (req, res) => {
-  res.sendFile(path.join(__dirname, 'auth', 'signup', 'signup.html'));
-});
+  app.get('/auth/register', (req, res) => {
+    res.sendFile(path.join(__dirname, 'auth', 'signup', 'signup.html'));
+  });
 
-app.get('/map', (req, res) => {
-  res.sendFile(path.join(__dirname, 'kakaomap', 'map.html'));
-});
+  app.get('/map', (req, res) => {
+    res.sendFile(path.join(__dirname, 'kakaomap', 'map.html'));
+  });
 
-app.get('/sheet', (req, res) => {
-  res.sendFile(path.join(__dirname, 'sheet', 'sheet.html'));
-});
+  app.get('/sheet', (req, res) => {
+    res.sendFile(path.join(__dirname, 'sheet', 'sheet.html'));
+  });
 
-// 서버 시작
-app.listen(3000, () => {
-  console.log('Server running on port 3000');
-});
+  // 서버 시작
+  app.listen(3000, () => {
+    console.log('Server running on port 3000');
+  });
