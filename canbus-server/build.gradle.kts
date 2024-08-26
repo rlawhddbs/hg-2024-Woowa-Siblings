272a7f05abe6512ce60fb9391118b42d5eb300ed
김종윤 <rlawhddbs@dgsw.hs.kr>
2024-08-26 13:50:39 +0900

    chore: Add JPA Plugin

--- canbus-server/build.gradle.kts
+++ canbus-server/build.gradle.kts
@@ -2,6 +2,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     kotlin("jvm") version "1.9.25"
+    kotlin("plugin.jpa") version "1.9.25"
     kotlin("plugin.spring") version "1.9.25"
     id("org.springframework.boot") version "3.3.3"
     id("io.spring.dependency-management") version "1.1.6"
